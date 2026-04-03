442907d285ce411922f667e2ee8e44df1d595870
m-tojo-safie <m-tojo@safie.jp>
2026-04-03 11:42:06 +0900

    fix zsh plugin

--- install.sh
+++ install.sh
@@ -56,8 +56,8 @@ fi
 
 # zshプラグインのインストール
 echo "🔌 zshプラグインをインストール中..."
-sudo -E apt update
-sudo -E apt install -y zsh-autosuggestions zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 echo "✅ zshプラグインをインストールしました"
 
 # GitHub CLIのインストール
